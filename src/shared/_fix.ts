198d10ccc4b3033539be8267dc4c72f8db02b6f6
JabDoesThings <JabDoesThings@github.com>
2022-04-14 08:28:23 -0500

    Added `_fix.ts` classes to fix issue of single populated source root compiling Lua files outside of its root folder.

---
+++ src/shared/_fix.ts
@@ -0,0 +1,6 @@
+// This file is left intentionally empty. 
+// (This is to fix a issue where files in only one directory compiles Lua file(s) 
+// outside of the client/server/shared folders)
+//
+// If you populate client/server/shared, you can remove this file. If you empty the
+// directory at some point, it might be a good idea to place this file back.
