fa9da1231c36c0f964bb21c0673f42739e1b83ea
Samantha Hughes <shughes.uk@gmail.com>
2022-08-14 00:54:16 -0700

    wip

--- scripts/reimport_template.lua
+++
@@ -1,9 +0,0 @@
--- PIPEWRENCH --
-if _G.Events.OnPipeWrenchBoot == nil then
-  _G.triggerEvent('OnPipeWrenchBoot', false)
-end
-_G.Events.OnPipeWrenchBoot.Add(function(____flag____)
-  if ____flag____ ~= true then return end
-  -- {IMPORTS}
-end)
-----------------
