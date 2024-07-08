cfa447dc52eefaa4789ae3f5a6021761f5a784b6
JabDoesThings <JabDoesThings@github.com>
2024-07-08 05:11:56 -0500

    Removed unused files. Ran `prettier`.

--- src/client/example/api/TSUIRedSquare.ts
+++ src/client/example/api/TSUIRedSquare.ts
@@ -1,27 +1,22 @@
-import { ISUIElement } from "@asledgehammer/pipewrench";
+import { ISUIElement } from '@asledgehammer/pipewrench';
 
 export class TSUIRedSquare extends ISUIElement {
+  constructor(x: number, y: number, width: number, height: number) {
+    super(x, y, width, height);
+    this.initialise();
+    this.instantiate();
+    this.addToUIManager();
+    this.setVisible(true);
+  }
 
-    constructor(x: number, y: number, width: number, height: number) {
-        super(x, y, width, height);
-        this.initialise();
-        this.instantiate();
-        this.addToUIManager();
-        this.setVisible(true);
-    }
+  render = (): void => {
+    this.drawRedSquare();
+  };
 
-    render = (): void => {
-        this.drawRedSquare();
-    }
-
-    /**
-     * Adds a red square element to the UI using the example ISUI typings.
-     */
-    drawRedSquare() {
-        this.drawRect(this.x, this.y, this.width, this.height, 1, 1.0, 0.0, 0.0);
-    }
-
-    emptyMethod() {
-        
-    } 
+  /**
+   * Adds a red square element to the UI using the example ISUI typings.
+   */
+  drawRedSquare() {
+    this.drawRect(this.x, this.y, this.width, this.height, 1, 1.0, 0.0, 0.0);
+  }
 }
